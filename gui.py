32ed587a84cd033a06eb546a728e395e8c2efe32
Pawan Paudel <paudelpawan17@gmail.com>
2024-08-07 20:11:30 -0500

    Implement Edit button

--- gui.py
+++ gui.py
@@ -2,8 +2,36 @@ import functions
 import FreeSimpleGUI
 
 label = FreeSimpleGUI.Text("Type in a to-do")
-input_box = FreeSimpleGUI.InputText(tooltip="Enter a todo")
+input_box = FreeSimpleGUI.InputText(tooltip="Enter a todo", key="todo")
 add_button = FreeSimpleGUI.Button("Add")
-window = FreeSimpleGUI.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
+list_box = FreeSimpleGUI.Listbox(values = functions.get_todos(), key="todos", enable_events = True, size=[45,10])
+edit = FreeSimpleGUI.Button("Edit")
+window = FreeSimpleGUI.Window("My To-Do App",
+                              layout=[[label], [input_box, add_button], [list_box, edit]],
+                              font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"]+"\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+            todos= functions.get_todos()
+            index= todos.index(todo_to_edit)
+            todos[index]= new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
+        case FreeSimpleGUI.WIN_CLOSED:
+            break
+
 window.close()
